04c0b7e9751ad71d035d0e66ff1499463ce8da45
Octavio Galland <ogalland@dc.uba.ar>
2023-07-31 13:41:53 -0300

    example script

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-#java -jar ../jatyc/checker/checker.jar -classpath ../jatyc/jatyc.jar -processor jatyc.JavaTypestateChecker *.java
-java -jar ~/java-typestate-checker/dist/checker/checker.jar -classpath ~/java-typestate-checker/dist/jatyc.jar -processor jatyc.JavaTypestateChecker *.java
+# FIXME: Specify the location of JaTyC on your host.
+JATYC_PATH=$HOME/java-typestate-checker
+
+java -jar $JATYC_PATH/dist/checker/checker.jar -classpath $JATYC_PATH/dist/jatyc.jar -processor jatyc.JavaTypestateChecker *.java
+
+run_server () {
+	# Run server for a little while
+	timeout 10 java FileServer
+}
+
+run_clients () {
+	sleep 1 # Give the server some time to initialize
+	java FileClient
+	java FileClient2
+}
+
+# Start the server and client in parallel
+run_server & run_clients
